4a28246eb3277beab30fa8440ed5c631f61102ca
Danny Francisco Castillo Solano <93727378+Dan4541@users.noreply.github.com>
2025-10-25 13:03:34 -0600

    Configuracion inicial

---
+++ Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre de Usuario es Obligatorio.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El Nombre de Usuario debe tener entre 3 y 50 caracteres")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "El Nombre Completo es Obligatorio.")]
+        [StringLength(100, ErrorMessage = "El Nombre Completo no debe exceder los 100 caracteres.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "El Correo Electrónico es Obligatorio.")]
+        [EmailAddress(ErrorMessage = "El Correo Electrónico no es válido.")]
+        [StringLength(100, ErrorMessage = "El Correo Electrónico no debe exceder los 100 caracteres.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La Contraseña es Obligatoria.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La Contraseña debe tener entre 6 y 100 caracteres.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "El Rol es Obligatorio.")]
+        [StringLength(20, ErrorMessage = "El Rol no debe exceder los 20 caracteres.")]
+        public string Role { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastAccess { get; set; }
+
+        // Navigation property to Professor
+        //public virtual Professor? Professor { get; set; }
+    }
+}
